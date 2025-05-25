a11d93ea84942f49e9a412589c73d9595806e7d6
mdasif8393 <md.asif.8393@gmail.com>
2025-05-25 13:52:30 +0600

    create 3 tables: rangers, species, sightings

--- assignment-2.sql
+++ assignment-2.sql
@@ -1,6 +1,23 @@
 -- create rangers table
 create table rangers(
     ranger_id INTEGER PRIMARY KEY,
-    name VARCHAR(50),
-    region VARCHAR (50)
+    name VARCHAR(50) NOT NULL,
+    region VARCHAR (50) NOT NULL
+)
+
+create table species(
+    species_id INTEGER PRIMARY KEY,
+    common_name VARCHAR(50) NOT NULL,
+    scientific_name VARCHAR(50) NOT NULL,
+    discovery_date DATE NOT NULL,
+    conservation_status VARCHAR(20) NOT NULL
+)
+
+create table sightings(
+    sighting_id INTEGER PRIMARY KEY,
+    ranger_id INTEGER REFERENCES rangers(ranger_id),
+    species_id INTEGER REFERENCES species(species_id),
+    sighting_time DATE NOT NULL,
+    location VARCHAR(50) NOT NULL,
+    notes VARCHAR(100),
 )
